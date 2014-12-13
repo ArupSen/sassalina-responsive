312c319c62d60360b50f5239963b731d0d9af287
Arup Sen <arupsen48@gmail.com>
2014-12-13 19:39:41 +0000

    added more items

---
+++ data/earrings_data.php
@@ -0,0 +1,30 @@
+<?php
+// -- earrings_data.php --
+
+// Only the file name needs to be different - the array names can be the same
+
+// back button is only used in the details pages
+$back_button = array(
+  '_href' => 'earrings.php', // relative url to main collection page
+  '_text' => 'earrings' // title attribute of back button and text
+);
+
+// array of image file names, which is also title of item and then blurb
+// array('heading text', 'the blurb');
+$items = array(
+  array('Kunzite and Praisolite Drop Earrings','A fantastically subtle colour combination which can be worn for day, whilst still being special enough to glitter beautifully at night time. Two pink Kunzite gems (7.13cts) and a perfect set of pale green Prasiolite drops (17.09cts) have been placed in an 18ct white gold frame with minimal metal to enhance the the beauty of the stones. These earrings can be recreated in other colour combinations.'),
+  array('white, cognac and champagne coloured Diamond Earrings','My client brought me a selection of different size and quality, old and new cut white diamonds and asked for a non white irregular design piece to go with a taupe wedding dress....We then selected cognac to champagne coloured lozenge shaped rough and brilliant cut Diamonds to be set into a textured 18ct white gold background.'),
+  array('Diamond and Pearl Earrings','I opened my post one morning and out dropped a well loved tear sheet from a magazine. The picture sent from Ireland was of a coveted pair of earrings. Already the proud owner of the gorgeous ruby and diamond ring (on my website) Sinead was confident to start the ball rolling from afar. Set in 18ct white gold the eight Diamonds weighing a total of approx .64 pts are perfectly balanced with 10mm pear shaped fresh water pearl drops.'),
+  array('Peridot and Mabe Pearl earrings','Beautiful cabochon peridot drop earrings with mabe pearls. Fantastic for everyday or evening wear. Available to be made in any colour and size as earrings and pendants.'),
+  array('South Sea Pearl Drop Earrings','Large South Sea Pearls. around I3mm wide, set with a very simple 18ct yellow gold hook. Pearls can be sourced in any size and colour and can be set simply or with Diamonds or coloured stone'),
+  array('Brilliant Cut Diamond Studs','Fantastic brilliant cut Diamond earrings totalling 1 ct set in 18ct yellow gold with white gold claws. Earrings can also be made with a rub over setting using your stones, or I can source any size to your budget.'),
+  array('Asscher Cut Diamond Studs','Another set of gorgeous Asscher cut Diamonds totalling over 2 cts, set in platinum.  They have GIA certificates stating E colour and VS1 clarity and have been set in a bezel or rub over style to contour the octagonal shape.'),
+  array('Natural Pink Diamond Earrings','It is always possible to create shoulders to be worn with all shapes of diamond stud, this set has 1 ct of untreated pink diamonds set in rose gold. There are endless possibilities in natural or treated diamonds and coloured gemstones and of course the could always be made to wear as a drop earring.'),
+  array('Chalcedony Rose and Moonstone Earrings','My client wanted earrings for every day which were predominantly white, organic and imperfect, not to be made from pearls or diamonds….She said we exceeded her expectations….A perfect combination of high quality blue sheen Moonstones with rare Chalcedony roses…I have a couple of pairs of the roses left….'),
+  array('Rutilated Quartz Earrings','This client really found it quite difficult to find drop earrings that suited her face perfectly, so I created a pair the perfect length. A very simple design using rutilated quartz which perfectly suits the yellow gold as it picks up the rods of golden colour in the ball. Easy to wear and great for everyday.'),
+  array('Peridot and Spessartitie Garnet Earrings','Having sourced a complete set of four matching peridot, we used the two pear drops to make earrings to compliment the pendant. It was very difficult to find the perfect pear shaped spessartites to match, so we found the best colour and approximate shape and had the stones cut to create the perfect balance to the drop. Once again all of the settings have been hand made to show minimal gold support.'),
+  array('Trilogy Diamond Studs','three stone diamond clover style studs set in 18ct yellow gold.'),
+  array('Diamond Hoop Earrings','Diamonds set in a small hoop style in 18ct white gold  which are great for everyday wear, with the ability to attach drops set with fab stones for evening or formal occasions.'),
+  array('Blue Topaz Studs','Fantastic Blue Topaz brilliant cut stones rub over set in 18ct gold, great for everyday and can be made in a any colour using a variety of stones.')
+);
+?>
