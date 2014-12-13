6d6c8b923f02cab44e7a2a86dde5a7ae5c965f94
Arup Sen <arupsen48@gmail.com>
2014-12-13 20:06:35 +0000

    first commit

---
+++ diamond_rings.php
@@ -0,0 +1,17 @@
+<?php
+// -- diamond_rings.php --
+
+// data will be fetched from data file
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+require_once DATAPATH.'diamond_rings_data.php'; // data file for this collection
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+<h2>DIAMOND RINGS</h2>
+<?php
+include_once INCPATH.'collection.php';
+?>
+
+<?php footer(); ?>
