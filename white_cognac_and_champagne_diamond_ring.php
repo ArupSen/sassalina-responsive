ac948976f71160e54decdc21412b459b61785b3c
Arup Sen <arupsen48@gmail.com>
2014-12-13 19:52:47 +0000

    first commit

---
+++ white_cognac_and_champagne_diamond_ring.php
@@ -0,0 +1,16 @@
+<?php
+// -- white_cognac_and_champagne_diamond_ring.php --
+
+// you need to require the data file with image and blurb
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+require_once DATAPATH.'diamond_rings_data.php'; // data file for this collection
+// index of piece in data file
+$piece = 4;
+navbar();
+
+// the unique contents will go here up to the footer
+include_once INCPATH.'details.php';
+?>
+
+<?php footer(); ?>
