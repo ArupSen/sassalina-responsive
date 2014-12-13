7c9b6f5187668ebffd249e027f0638ae683b31a0
Arup Sen <arupsen48@gmail.com>
2014-12-13 19:40:27 +0000

    added one item

---
+++ data/necklaces_data.php
@@ -0,0 +1,32 @@
+<?php
+// data/necklaces_data.php
+
+// Only the file name needs to be different - the array names can be the same
+
+// back button is only used in the details pages
+$back_button = array(
+  '_href' => 'necklaces.php', // relative url to main collection page
+  '_text' => 'necklaces' // title attribute of back button and text
+);
+
+// array of image file names, which is also title of item and then blurb
+// array('heading text', 'the blurb');
+$items = array(
+  array('Diamond Pendant with detachable Sapphire tassel','The brief was to incorporate a diamond brooch into a wearable necklace. The beauty of this piece is that the Sapphire bead tassel has been cleverly created to be removable. This enables the necklace to be worn more simply without the drop or with a multitude of different coloured stones or pearls as an attachment.'),
+  array('Handmade Platinum Graduated Diamond Necklace','This piece speaks for itself, however it would have appeared even more magnificent than in the photo if it had been modelled on the gorgeous tanned skin of its\' owner. A perfectly graduated collection of high quality diamonds with a fabulous claps which cleverly hide the fixing mechanism set in a handmade platinum mount.'),
+  array('Emerald and Tanzanite Pendant','This 14ct cabochon Emerald is paired with a 5.35ct oval cabochon Tanzanite to bring out the richness of the colours, set in 18ct yellow gold,  I can make similar pendants with any combination of stones....'),
+  array('Periodot and Spessartite Pendant','A unique pendant made from a flawless 25ct peridot and a 6 ct spessartite garnet. the setting is hand made to use the minimum about of gold, allowing the stones to speak for themselves. The 18ct yellow gold cable was made to match the piece.'),
+  array('Aquamarine Bead Necklace','Gorgeous faceted Roundels of Aquamarine beads threaded with vibrant neon orange silk to enhance the colour and make a modern statement.'),
+  array('Multi-coloured Tourmaline Pendant','A completely unique pendant set with 19 different coloured Tourmalines The lovely mix of soft hued stones works well, however this piece can be made with any number of colours or mix of coloured stones and could be made as a pendant, ring, earrings or bracelet.'),
+  array('Multi-coloured Sapphire Pendant','Warm and rich colours of the Sapphire spectrum wrapped luxuriously in 18ct yellow gold makes this pendant smooth to touch and very easy to wear every day.'),
+  array('Multi-coloured Sapphire Pendant silver','I absolutely adore the diversity of coloured sapphires and this very soft palate of rose cut stones has been set in a barely there 9ct white gold setting to show off the stones to their best. Look under Gems to see additional sets waiting to be made up should you be tempted!'),
+  array('Rough Cut Amethyst bead Necklace','Chunky rough cut Amethyst beads threaded to make a fantastic necklace secured with a magnetic clasp…this piece is ready for sale.'),
+  array('Spinel and Pearl Pendant','During a trip to Oz, I bought this large tear drop shaped south sea pearl...My client spotted it amongst my box of treasures and chose a 4.04ct purplespinel and we set them together in this classically simple 18ct white gold necklace.'),
+  array('Diamond Polo Pendant','A very good client of mine had a rather tired ring with two fans of baguette shaped diamonds she never wore….after much discussion she agreed a pendant for everyday was what she needed…this design only required a minimal number of small diamonds to be added and now she has a gorgeous piece to enjoy! '),
+  array('Double Diamond drop Pendant','I met this client on a sailing holiday...she was talking about two solitaire diamond rings she had inherited and never wore and I suggested a simple double stone pendant in 18ct white gold, which she now wears everyday...recycle diamonds..'),
+  array('Diamond Horseshoe Pendant','The horse shoe shaped Diamond brooch came into my family in the late 1800\'s and as brooch wearing is not in fashion, it rarely had an outing. I purchased an antique chain and had it set mid-centre and viola a new diamond pendant for me to enjoy. Have a think if you have any old brooches collecting dust?'),
+  array('Gold and Diamond Star Pendant','My client had inherited a very sentimental diamond engagement ring form her grandmother but had no need for another ring, I knew she liked stars and so I suggested we design one adding a few additional diamonds to bring this sparkly star to life….'),
+  array('Personalised diamond and gold necklace','Graduating from High School and leaving London to go College in the States is a big event in anyones life…to celebrate the occasion two bespoke necklaces were made with nicknames the Mother and Daughter had for each other….I only have a photo of one as the other is already transatlantic!'),
+  array('Solitaire Diamond Pendant','This cute pendant was set using an inherited Victorian diamond ring that was no longer usable due to wear and tear')
+);
+?>
