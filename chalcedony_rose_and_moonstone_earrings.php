7082e08b91b7a476c2711076b5a7237ae93ab057
Arup Sen <arupsen48@gmail.com>
2014-12-13 19:43:37 +0000

    added testimonial but not from data file

---
+++ chalcedony_rose_and_moonstone_earrings.php
@@ -0,0 +1,18 @@
+<?php
+// chalcedony_rose_and_moonstone_earrings
+
+// you need to require the data file with image and blurb
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+require_once DATAPATH.'earrings_data.php'; // data file for this collection
+navbar();
+
+// the unique contents will go here up to the footer
+include_once INCPATH.'details.php';
+?>
+<div class="testimonial">
+<h4>Francesca G Testimonial</h4>
+<p>“When I met with Sarah I had a clear idea of what I wanted to commission, a pair of earrings that could  be worn everyday and to be made from pale/white stones. However I did not want to use pearls or diamonds, as I really wanted them to be organic and unusual in design.</p>
+<p>Sarah suggested using some semi precious gems that I had never heard of, very unusual and exactly what I wanted. She also held my hand throughout the design process ensuring I was happy with every detail.  I am delighted with the result and use the earrings even more than I expected!”</p>
+</div>
+<?php footer(); ?>
