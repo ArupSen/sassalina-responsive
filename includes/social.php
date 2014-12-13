b2d9a50d973fd59da4cb46e32b2a70f8ecc08481
Arup Sen <arupsen48@gmail.com>
2014-12-13 19:57:36 +0000

    first commit

---
+++ includes/social.php
@@ -0,0 +1,10 @@
+<?php
+// social networking link icons for header and footer sections
+?>
+<div class="social-icons">
+<a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+<a href="https://twitter.com/SassalinaGems" target="_blank"><i class="fa fa-twitter"></i></a>
+<a href="#" target="_blank"><i class="fa fa-instagram"></i></a>
+<a href="http://sassalina.wordpress.com/" target="_blank"><i class="fa fa-wordpress"></i></a>
+<a href="#" target="_blank"><i class="fa fa-pinterest"></i></a>
+</div>
---
+++ includes/testimonial.php
@@ -0,0 +1,9 @@
+<?php
+// snippet that gets data from the testimonials data file
+// the testimonials array is 2 dimensional with the inner array having 3 elements
+?>
+<div class="testimonial">
+<h4><?php echo $testimonials[$name][0]; ?> Testimonial</h4>
+<p>(several pieces commissioned)</p>
+<p><?php echo $testimonials[$name][1]; ?></p>
+</div>
