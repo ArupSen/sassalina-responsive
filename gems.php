7bec6c111ce516b34c0c5e4905be073a0754ee97
Arup Sen <arupsen48@gmail.com>
2014-12-13 20:04:23 +0000

    first commit

---
+++ gems.php
@@ -0,0 +1,17 @@
+<?php
+// gems.php
+
+// data will be fetched from data file
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+require_once DATAPATH.'gems_data.php'; // data file for this collection
+navbar();
+
+// the unique contents will go here up to the footer
+?>
+<h2>GEMS</h2>
+<?php
+include_once INCPATH.'collection.php';
+?>
+
+<?php footer(); ?>
---
+++ peach_moonstone_cufflinks.php
@@ -0,0 +1,16 @@
+<?php
+// -- peach_moonstonse_cufflinks.php --
+
+// you need to require the data file with image and blurb
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+require_once DATAPATH.'cufflinks_data.php'; // data file for this collection
+// index of piece in data file
+$piece = 4;
+navbar();
+
+// the unique contents will go here up to the footer
+include_once INCPATH.'details.php';
+?>
+
+<?php footer(); ?>
