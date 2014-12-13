93f0a2150255d2a361ae4e2ed82c9d992ebc8955
Arup Sen <arupsen48@gmail.com>
2014-12-13 19:38:27 +0000

    added more items

---
+++ data/diamond_rings_data.php
@@ -0,0 +1,28 @@
+<?php
+// -- diamond_rings_data.php --
+
+// Only the file name needs to be different - the array names can be the same
+
+// back button is only used in the details pages
+$back_button = array(
+  '_href' => 'diamond_rings.php', // relative url to main collection page
+  '_text' => 'diamond rings' // title attribute of back button and text
+);
+
+// array of image file names, which is also title of item and then blurb
+// array('heading text', 'the blurb');
+$items = array(
+  array('Tapered Baguette and Diamond Ring','A perfect treasure to celebrate a 25th Wedding anniversary, my clients enjoyed the process of choosing the central Diamond from a selection of \'collection quality\' stones weighing between 1.10-2.02cts.  The final choice was a beautiful 1.6cts F colour VS1 with GIA certs and styled with  0.61pts of tapered baguettes set in Platinum.'),
+  array('Diamond Pave Set Ring','A lovely friend wanted to surprise his wife with a very sparkly diamond ring for her 40th. He had photo\'s of rings he liked, we worked together to choose the most stunning of all the options, weighing a total of 4.12cts set in Platinum. Finally we did have to size her finger as a ring with stones all the way around is very expensive to resize!'),
+  array('Baguette Cut Diamond Eternity Band','The most amazing full Diamond eternity band, set in platinum with a total of 6.4cts of Diamonds. The special story with this one is that my client lost the ring after one year, so I have made it twice! This style of ring can also be made with square or round stones and in a half or third show of stones for the more cost conscious.'),
+  array('Untreated Pink and White Diamond Ring','I loved using my client\'s solitaire engagement ring to create a wide banded ring, which was not heavy, but modern and timeless....I suggested coloured diamonds, we considered pink, yellow, raspberry and teal, but the natural pink were the winner from the start....Set in 18ct white gold and using .96pts of pink diamonds, the wrapping design was inspired by arms holding safe the sentimental stone.'),
+  array('White, Cognac and Champagne Diamond Ring','Each piece has a completely individual lay out of stones, but with an aesthetic balance. There is a lovely dome shape and the 3D stones show the light beautifully when worn. We took the time to discuss each stage of the process, the result being a wedding dress perfectly complemented by a bespoke collection.'),
+  array('Brilliant Cut Diamond set in Platinum','An unusual request from a client who describes herself as \'not a big jewellery wearer\' to set a rather impressive 2.2ct Brilliant cut Diamond in as simple a setting as possible. To find the solution we enjoyed shopping together to really help choose the best setting for her, the result is the perfect modern and simple bezel set Platinum Solitaire.'),
+  array('Brilliant Cut Diamond set in Yellow Gold','A 2.5cts Diamond ring with gold band and white gold setting, specifically made to sit very low on the finger.'),
+  array('Princess Cut Solitaire Diamond ring','A beautiful 1ct Princess cut Diamond with an EGL certificate set in Platinum for a very romantic surprise proposal under the stars whilst on holiday in the Middle East and the great news is that Rachel was thrilled!'),
+  array('Memories Diamond Ring','A \'Memories Ring\' was created from my client\'s 24ct gold grandmother\'s wedding band, her mother\'s engagement and eternity rings…the brief was to make one ring to wear everyday and as a woman who has worked her whole life in the fashion industry, her personal style was a very important element to the brief!'),
+  array('Gypsy set Diamond Ring','A 0.75ct diamond taken from a Victorian engagement setting and made into a contemporary ring in 18ct gold to be worn in a modern way.'),
+  array('Diamond Solitaire ring','This is the most touching story of a couple who bought their engagement ring in 1949, 60 years later he wanted to buy his lovely wife a Diamond ring...The brief was to make .74pts diamond into a ring which sat perfectly with her wide gold wedding band.'),
+  array('Princess Cut Diamond with Diamond set Band','Gorgeous solitaire diamond which I enhanced by setting with carre cut diamonds set into the shank to add sparkle and elegance to the ring!')
+);
+?>
