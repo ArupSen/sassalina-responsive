4e10b2a9fc3968e9f76db4482895b562f84083ca
Arup Sen <arupsen48@gmail.com>
2014-12-14 21:07:32 +0000

    added two new pieces

--- data/bracelets_data.php
+++ data/bracelets_data.php
@@ -12,6 +12,8 @@ $back_button = array(
 // array of image file names, which is also title of item and then blurb
 // array('heading text', 'the blurb');
 $items = array(
-  array('Gold and Diamond Bangle','This is a very sentimental piece which I named the \'memories bangle\' it incorporates gold and diamonds from important people in my clients life, which now can be kept close to her and enjoyed everyday.')
+  array('Gold and Diamond Bangle','This is a very sentimental piece which I named the \'memories bangle\' it incorporates gold and diamonds from important people in my clients life, which now can be kept close to her and enjoyed everyday.'),
+  array('Diamond Tennis Bracelet Platinum','A stunning diamond tennis bracelet made from platinum and set with approximately 47 stones weighing a total of approx 6 carats.'),
+  array('Diamond Tennis Bracelet','My client found that with wear her diamond tennis bracelet had stretched and the metal had rubbed though, with a piece of this calibre it was very important to find the perfect size mount and we spent time customising the clasp to her specification.')
 );
 ?>
