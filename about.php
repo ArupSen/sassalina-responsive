82d4639f36173749f466445dca97117a440e8c15
Arup Sen <arup@arupdesigns.co.uk>
2014-12-15 14:04:52 +0000

    added the logo images and added bootstrap rows

--- about.php
+++ about.php
@@ -6,15 +6,27 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
-<div class="col-md-6 about-pic">
-<?php img_tag('about.jpg'); ?>
-</div>
-  <div class="col-md-6">
-    <p>
-Sassalina was born from a life-long passion for jewellery and gems.  As a teenager my parents encouraged me to design jewellery incorporating inherited gems and I still wear these pieces today. I believe if the bespoke design perfectly suits the style of the wearer the jewellery will be timeless and the memories held within the piece very powerful.  
-</p>
-<p>After having my three children, I was determined to have a second career based on my passion. I started by attending holiday courses at Central St Martin’s in jewellery making.   Knowledge of making is essential to design, however quite quickly I knew I wanted to become a Gemmologist.  In 2008 I returned to full time study and become a Fellow of the Gemological Association of Great Britain.  I have also attended GIA courses in jewellery drawing. I have been designing for myself since 1983 and for clients since 2006.
-</p>
+<div class="row">
+  <div class="col-md-6 about-pic">
+  <?php img_tag('about.jpg'); ?>
   </div>
-
+  <div class="col-md-6">
+  <p>
+  Sassalina was born from a life-long passion for jewellery and gems.  As a teenager my parents encouraged me to design jewellery incorporating inherited gems and I still wear these pieces today. I believe if the bespoke design perfectly suits the style of the wearer the jewellery will be timeless and the memories held within the piece very powerful.  
+  </p>
+  <p>After having my three children, I was determined to have a second career based on my passion. I started by attending holiday courses at Central St Martin’s in jewellery making.   Knowledge of making is essential to design, however quite quickly I knew I wanted to become a Gemmologist.  In 2008 I returned to full time study and become a Fellow of the Gemological Association of Great Britain.  I have also attended GIA courses in jewellery drawing. I have been designing for myself since 1983 and for clients since 2006.
+  </p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-md-4">
+  <?php img_tag('slf_logo.jpg'); ?>
+  </div>
+  <div class="col-md-4">
+  <?php img_tag('member_coa.jpg'); ?>
+  </div>
+  <div class="col-md-4">
+  <?php img_tag('leopard_head.jpg'); ?>
+  </div>
+</div>
 <?php footer(); ?>
