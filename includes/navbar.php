76bdeb3e9b25cdd635c5abd4f9457dd481094b66
Arup Sen <arup@arupdesigns.co.uk>
2014-12-15 15:22:36 +0000

    renamed one link and changed order of nav items

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,13 +7,13 @@
 	<li id="nav-about"><a href="about.php" title="about">about</a></li>
   <li id="nav-bespoke" class="dropdown"><a href="#" title="bespoke" class="dropdown-toggle" data-toggle="dropdown">bespoke</a>
     <ul class="dropdown-menu" role="menu">
-      <li><a href="coloured_rings.php" title="Coloured Rings">Coloured Rings</a></li>
+      <li><a href="coloured_rings.php" title="Gemstone Rings">Gemstone Rings</a></li>
       <li><a href="diamond_rings.php" title="Diamond Rings">Diamond Rings</a></li>
+      <li><a href="earrings.php" title="Earrings">Earrings</a></li>
       <li><a href="necklaces.php" title="Necklaces">Necklaces</a></li>
       <li><a href="bracelets.php" title="Bracelets">Bracelets</a></li>
       <li><a href="cufflinks.php" title="Cufflinks">Cufflinks</a></li>
       <li><a href="gems.php" title="Gems">Gems</a></li>
-      <li><a href="earrings.php" title="Earrings">Earrings</a></li>
     </ul>
   </li>
 	<li id="nav-design"><a href="design.php" title="design">design</a></li>
