9e1d6340c56aa169e25e5d406c8eaba4d7abf2e3
Arup Sen <arup@arupdesigns.co.uk>
2014-12-15 17:42:57 +0000

    added content to page

--- design.php
+++ design.php
@@ -7,7 +7,39 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
-
-
-
+<div class="row">
+  <div class="col-md-8">
+<?php img_tag('Alyandsarah_smaller.jpg'); ?>
+  </div>
+  <div class="col-md-4">
+<h4>BESPOKE</h4>
+<p>Every client's requirements are unique and my job is to make your dream come true….with some expert advice to avoid some of the pitfalls. I believe designing the perfect piece is often a blend of connecting with the emotional reasons for a commission whilst taking into account the physical features of the wearer and practical elements of how the piece will be worn.</p>
+<p>
+After considering your budget, I will choose a selection of stones for you to peruse. The gem may be rare one of a kind, or you may have several stones to select from.  We then spend as long as you need considering different designs in order to make the dream a reality.</p>
+  </div>
+</div>
+<hr>
+<div class="row">
+  <div class="col-md-4">
+<h4>REDESIGN</h4>
+<p>Very often I find myself listening to a friend or colleague talking about inheriting a piece of jewellery that they would never wear.  For me, this is the perfect chance to show how something fabulous can be created in a wearable style unique to the client.</p>
+<p>Remodelling jewellery is typically very affordable as you are starting with the basis for the piece.  I have included on this page a few client testimonials with completely different starting points, all of whom are the delighted recipients of new pieces using old stones. I can also help with repairing well-loved jewellery.
+</p>
+  </div>
+  <div class="col-md-4">
+<?php img_tag('before_design.jpg'); ?>
+<p class="before-text">BEFORE</p>
+  </div>
+  <div class="col-md-4">
+<?php img_tag('after_design.jpg'); ?>
+<p class="after-text">AFTER</p>
+  </div>
+</div>
+<div class="row testimonial-show">
+<h4>READ PHILLIPA’S TESTIMONIAL HERE</h4>
+<div class="show-hide">
+<p>"When an elderly aunt died a couple of years ago, leaving me a turquoise and gold brooch in her will, I have to admit that I was quite excited to see what it looked like.  Sadly, the brooch was quite ugly; a dome of tiny turquoise beads surrounded by two elaborate snakes with gold tassels hanging from either side. Knowing that I was never going to wear it, I took it to Sarah to see what she thought.  Straight away, Sarah could see that without its gold surround, the dome of turquoise beads could look well as a pendant on a fine gold chain. The end result is a unique and very pretty necklace.  I wear it often and whenever I do, it attracts compliments. I’m so happy to be able to wear something that belonged to my aunt and I don’t think she would have minded at all that the brooch has been adapted to suit the tastes of a different generation."
+</p>
+</div>
+</div>
 <?php footer(); ?>
