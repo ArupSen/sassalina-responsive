3ec0596c25a6eafc2ced4d555eb9cd27eb839891
Arup Sen <arupsen48@gmail.com>
2014-12-19 12:22:41 +0000

    added bootstrap class to testimonial section

--- design.php
+++ design.php
@@ -35,7 +35,7 @@ After considering your budget, I will choose a selection of stones for you to pe
 <p class="after-text">AFTER</p>
   </div>
 </div>
-<div class="row testimonial-show">
+<div class="row testimonial-show col-md-12">
 <h4>READ PHILLIPA’S TESTIMONIAL HERE</h4>
 <div class="show-hide">
 <p>"When an elderly aunt died a couple of years ago, leaving me a turquoise and gold brooch in her will, I have to admit that I was quite excited to see what it looked like.  Sadly, the brooch was quite ugly; a dome of tiny turquoise beads surrounded by two elaborate snakes with gold tassels hanging from either side. Knowing that I was never going to wear it, I took it to Sarah to see what she thought.  Straight away, Sarah could see that without its gold surround, the dome of turquoise beads could look well as a pendant on a fine gold chain. The end result is a unique and very pretty necklace.  I wear it often and whenever I do, it attracts compliments. I’m so happy to be able to wear something that belonged to my aunt and I don’t think she would have minded at all that the brooch has been adapted to suit the tastes of a different generation."
