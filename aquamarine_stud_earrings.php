7e1906cfa67a2feaf56ee9cfb1de0176e303f10e
Arup Sen <arupsen48@gmail.com>
2015-01-19 17:14:14 +0000

    added sally testimonial

--- aquamarine_stud_earrings.php
+++ aquamarine_stud_earrings.php
@@ -5,10 +5,13 @@
 require_once 'inc_path.php';
 require_once INCPATH.'header.php';
 require_once DATAPATH.'earrings_data.php'; // data file for this collection
+require_once DATAPATH.'testimonials_data.php';
+$name = 'sally';
 navbar();
 
 // the unique contents will go here up to the footer
 include_once INCPATH.'details.php';
+include_once INCPATH.'testimonial.php';
 ?>
 
 <?php footer(); ?>
