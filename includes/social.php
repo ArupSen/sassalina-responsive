f0949ce79c883680e62b4e738d75afc05faf9c27
Arup Sen <arupsen48@gmail.com>
2015-01-19 16:39:07 +0000

    added missing urls

--- includes/social.php
+++ includes/social.php
@@ -2,9 +2,9 @@
 // social networking link icons for header and footer sections
 ?>
 <div class="social-icons">
-<a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+<a href="https://www.facebook.com/SassalinaGems?fref=ts" target="_blank"><i class="fa fa-facebook"></i></a>
 <a href="https://twitter.com/SassalinaGems" target="_blank"><i class="fa fa-twitter"></i></a>
-<a href="#" target="_blank"><i class="fa fa-instagram"></i></a>
+<a href="http://instagram.com/sassalinabespoke" target="_blank"><i class="fa fa-instagram"></i></a>
 <a href="http://sassalina.wordpress.com/" target="_blank"><i class="fa fa-wordpress"></i></a>
-<a href="#" target="_blank"><i class="fa fa-pinterest"></i></a>
+<a href="http://www.pinterest.com/sassalinagems/sassalina-jewellery/" target="_blank"><i class="fa fa-pinterest"></i></a>
 </div>
