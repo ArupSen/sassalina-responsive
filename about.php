e096db0e4640835c3f6c810ee60a0caeebe0c814
Arup Sen <arupsen48@gmail.com>
2015-01-21 14:57:33 +0000

    PR email added and swapped main image

--- about.php
+++ about.php
@@ -8,7 +8,7 @@ navbar();
 ?>
 <div class="row">
   <div class="col-md-6 about-pic">
-  <?php img_tag('about.jpg'); ?>
+  <?php img_tag('sarah_smaller.jpg'); ?>
   </div>
   <div class="col-md-6">
   <p>
@@ -16,6 +16,7 @@ navbar();
   </p>
   <p>After having my three children, I was determined to have a second career based on my passion. I started by attending holiday courses at Central St Martin’s in jewellery making.   Knowledge of making is essential to design, however quite quickly I knew I wanted to become a Gemmologist.  In 2008 I returned to full time study and become a Fellow of the Gemological Association of Great Britain.  I have also attended GIA courses in jewellery drawing. I have been designing for myself since 1983 and for clients since 2006.
   </p>
+<p>Caroline Crossland (PR) <a href="mailto:caroline@sassalina.com">caroline@sassalina.com</a></p>
   </div>
 </div>
 <div class="row logos">
