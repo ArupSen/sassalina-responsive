d6e4f4817c8c4cb20e2a64ee6461d2ba5035d939
Arup Sen <arupsen48@gmail.com>
2015-01-27 12:04:13 +0000

    removed caroline crossland email

--- about.php
+++ about.php
@@ -16,7 +16,6 @@ navbar();
   </p>
   <p>After having my three children, I was determined to have a second career based on my passion. I started by attending holiday courses at Central St Martin’s in jewellery making.   Knowledge of making is essential to design, however quite quickly I knew I wanted to become a Gemmologist.  In 2008 I returned to full time study and become a Fellow of the Gemological Association of Great Britain.  I have also attended GIA courses in jewellery drawing. I have been designing for myself since 1983 and for clients since 2006.
   </p>
-<p>Caroline Crossland (PR) <a href="mailto:caroline@sassalina.com">caroline@sassalina.com</a></p>
   </div>
 </div>
 <div class="row logos">
