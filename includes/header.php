0f36e6459f75df9608e6001b1e599e931a322da2
Arup Sen <arupsen48@gmail.com>
2015-01-27 12:06:54 +0000

    dumped in slim mail chimp form

--- includes/header.php
+++ includes/header.php
@@ -44,6 +44,26 @@ include_once DATAPATH.'meta_tags.php';
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+<div id="mc_embed_signup">
+<form action="//sassalina.us9.list-manage.com/subscribe/post?u=4447e56fae25233ff8fef3421&amp;id=a49cbeed4e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+	
+	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+    <div style="position: absolute; left: -5000px;"><input type="text" name="b_4447e56fae25233ff8fef3421_a49cbeed4e" tabindex="-1" value=""></div>
+    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    </div>
+</form>
+</div>
+
+<!--End mc_embed_signup-->
 <?php
 include INCPATH.'social.php';
 ?>
