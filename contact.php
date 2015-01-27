605c57dbeec3d286fef9aa0e13f96957fcb872d6
Arup Sen <arupsen48@gmail.com>
2015-01-27 12:04:49 +0000

    added contact page content

--- contact.php
+++ contact.php
@@ -6,6 +6,23 @@ navbar();
 
 // the unique contents will go here up to the footer
 ?>
+<div class="row">
+  <div class="col-md-6 about-pic">
+  <?php img_tag('contact.jpg'); ?>
+  </div>
+  <div class="col-md-4">
+<h3>CONTACT</h3>
+  <p>
+Feel free to contact me and we can have a chat about
+gemstones, jewellery repair, redesigning  or if you're interested
+in having a bespoke piece made up...no obligation...
+  </p>
+<p>Sarah Freeman FGA<br />
+<a href="mailto:sarah@sassalina.com">sarah@sassalina.com</a><br />
+Ph: +44(0)7866 516 281<br />
 
-
+Based in London SW12</p>
+<p>Caroline Crossland (PR) <br /><a href="mailto:caroline@sassalina.com">caroline@sassalina.com</a></p>
+  </div>
+</div>
 <?php footer(); ?>
