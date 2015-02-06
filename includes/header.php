5a6198af4e39029ca1c13cf50cf1925b22eb68cf
Arup Sen <arupsen48@gmail.com>
2015-02-06 13:26:54 +0000

    updated dummy sign-up form with real one from mail chimp

--- includes/header.php
+++ includes/header.php
@@ -38,27 +38,29 @@ include_once DATAPATH.'meta_tags.php';
 <div id="wrap" class="container">
 <div class="contact-bar">
 <a href="contact.php" class="contact-link">contact</a>
+<!--
  <form class="navbar-form navbar-left" role="search">
         <div class="form-group">
           <input type="text" class="form-control" placeholder="Sign up to our Newsletter" size="21">
         </div>
         <button type="submit" class="btn btn-default">Submit</button>
       </form>
+-->
 <!-- Begin MailChimp Signup Form -->
-<link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
+<!-- <link href="//cdn-images.mailchimp.com/embedcode/slim-081711.css" rel="stylesheet" type="text/css">
 <style type="text/css">
 	#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
 	/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
 	   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
-</style>
+</style> -->
 <div id="mc_embed_signup">
-<form action="//sassalina.us9.list-manage.com/subscribe/post?u=4447e56fae25233ff8fef3421&amp;id=a49cbeed4e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-    <div id="mc_embed_signup_scroll">
-	
-	<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+<form action="//sassalina.us9.list-manage.com/subscribe/post?u=4447e56fae25233ff8fef3421&amp;id=a49cbeed4e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate navbar-form navbar-left" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll" class="form-group">
+
+  <input type="email" value="" name="EMAIL" class="form-control email" id="mce-EMAIL" placeholder="Sign up to our Newsletter" required size="24">
     <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
     <div style="position: absolute; left: -5000px;"><input type="text" name="b_4447e56fae25233ff8fef3421_a49cbeed4e" tabindex="-1" value=""></div>
-    <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-default">
     </div>
 </form>
 </div>
