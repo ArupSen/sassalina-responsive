235eb983e62ac92e47bc6d36251a1153449c5666
Arup Sen <arupsen48@gmail.com>
2015-02-06 13:25:33 +0000

    changed title to h4 and rearranged text

--- contact.php
+++ contact.php
@@ -11,7 +11,7 @@ navbar();
   <?php img_tag('contact.jpg'); ?>
   </div>
   <div class="col-md-4">
-<h3>CONTACT</h3>
+<h4>CONTACT</h4>
   <p>
 Feel free to contact me and we can have a chat about
 gemstones, jewellery repair, redesigning  or if you're interested
@@ -20,9 +20,9 @@ in having a bespoke piece made up...no obligation...
 <p>Sarah Freeman FGA<br />
 <a href="mailto:sarah@sassalina.com">sarah@sassalina.com</a><br />
 Ph: +44(0)7866 516 281<br />
-
-Based in London SW12</p>
+</p>
 <p>Caroline Crossland (PR) <br /><a href="mailto:caroline@sassalina.com">caroline@sassalina.com</a></p>
+<p>Based in London SW12</p>
   </div>
 </div>
 <?php footer(); ?>
