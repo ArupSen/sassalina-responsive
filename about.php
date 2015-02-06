f68cb9265204a2304e2dc7c4cc998d9c50f13f7c
Arup Sen <arupsen48@gmail.com>
2015-02-06 13:24:41 +0000

    added classes to each img div

--- about.php
+++ about.php
@@ -16,16 +16,17 @@ navbar();
   </p>
   <p>After having my three children, I was determined to have a second career based on my passion. I started by attending holiday courses at Central St Martin’s in jewellery making.   Knowledge of making is essential to design, however quite quickly I knew I wanted to become a Gemmologist.  In 2008 I returned to full time study and become a Fellow of the Gemological Association of Great Britain.  I have also attended GIA courses in jewellery drawing. I have been designing for myself since 1983 and for clients since 2006.
   </p>
+<p>Sarah Freeman</p>
   </div>
 </div>
 <div class="row logos">
-  <div class="col-md-4">
+  <div class="col-md-4 slf">
   <?php img_tag('slf_logo.jpg'); ?>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-4 gem-soc">
   <?php img_tag('member_coa.jpg'); ?>
   </div>
-  <div class="col-md-4">
+  <div class="col-md-4 leopard">
   <?php img_tag('leopard_head.jpg'); ?>
   </div>
 </div>
