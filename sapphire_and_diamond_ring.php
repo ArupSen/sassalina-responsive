449e39b4af70eb300e0d09efeffee9b74ffdc1ec
Arup Sen <arupsen48@gmail.com>
2015-02-27 16:51:26 +0000

    added maxwell testimonial

--- sapphire_and_diamond_ring.php
+++ sapphire_and_diamond_ring.php
@@ -5,12 +5,13 @@
 require_once 'inc_path.php';
 require_once INCPATH.'header.php';
 require_once DATAPATH.'coloured_rings_data.php'; // data file for this collection
-// index of piece in data file
-$piece = 4;
+require_once DATAPATH.'testimonials_data.php';
 navbar();
 
 // the unique contents will go here up to the footer
 include_once INCPATH.'details.php';
+$name = 'maxwell';
+include_once INCPATH.'testimonial.php';
 ?>
 
 <?php footer(); ?>
