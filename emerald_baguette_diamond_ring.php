32c998557655197f88a2957d6007fbf237fe077b
Arup Sen <arupsen48@gmail.com>
2015-02-27 16:52:16 +0000

    initial commit

---
+++ emerald_baguette_diamond_ring.php
@@ -0,0 +1,15 @@
+<?php
+// emerald_and_baguette_diamond_ring.php
+
+
+// you need to require the data file with image and blurb
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+require_once DATAPATH.'coloured_rings_data.php'; // data file for this collection
+navbar();
+
+// the unique contents will go here up to the footer
+include_once INCPATH.'details.php';
+?>
+
+<?php footer(); ?>
---
+++ marquise_diamond_ring_with_tapered_baguettes.php
@@ -0,0 +1,13 @@
+<?php
+// marquise_diamond_ring_with_tapered_baguettes.php
+
+// you need to require the data file with image and blurb
+require_once 'inc_path.php';
+require_once INCPATH.'header.php';
+require_once DATAPATH.'diamond_rings_data.php'; // data file for this collection
+navbar();
+// the unique contents will go here up to the footer
+include_once INCPATH.'details.php';
+?>
+
+<?php footer(); ?>
